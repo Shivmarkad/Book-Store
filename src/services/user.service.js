0c6daa19e1a201edd363bf8cfeb551ba53da16c5
Shivganesh <shivmarkad99@gmail.com>
2023-09-02 21:10:39 +0530

    [Shivganesh] Created: User SignUp API

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,39 +1,13 @@
 import User from '../models/user.model';
 
-//get all users
-export const getAllUsers = async () => {
-  const data = await User.find();
-  return data;
-};
+//SignUp new user
+export const signUp = async (body) => {
+  const user = await User.findOne({email: body.email});
 
-//create new user
-export const newUser = async (body) => {
+  if(user){
+    throw new Error("You are already registered !!")
+  }
+  
   const data = await User.create(body);
-  return data;
-};
-
-//update single user
-export const updateUser = async (_id, body) => {
-  const data = await User.findByIdAndUpdate(
-    {
-      _id
-    },
-    body,
-    {
-      new: true
-    }
-  );
-  return data;
-};
-
-//delete single user
-export const deleteUser = async (id) => {
-  await User.findByIdAndDelete(id);
-  return '';
-};
-
-//get single user
-export const getUser = async (id) => {
-  const data = await User.findById(id);
   return data;
 };
