0d323affc4f01ae15e2001a957c8e9deaee08a24
Shivganesh <shivmarkad99@gmail.com>
2023-09-02 21:55:45 +0530

    [Shivganesh] Refactored: Using bcrypt for hashing the password while registration

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,13 +1,21 @@
 import User from '../models/user.model';
+import bcrypt from 'bcrypt';
 
 //SignUp new user
 export const signUp = async (body) => {
-  const user = await User.findOne({email: body.email});
+  const user = await User.findOne({ email: body.email });
 
-  if(user){
+  if (user) {
     throw new Error("You are already registered !!")
   }
-  
+
+  const saltRounds = 10;
+  const hash = bcrypt.hashSync(body.password, saltRounds);
+  body.password = hash;
+
   const data = await User.create(body);
-  return data;
+
+  const {firstName, lastName, email} = data;
+
+  return {firstName, lastName, email};
 };
