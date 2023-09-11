8f87bcf1c1e9aae3399eeb852486b76689c83b3d
Shivganesh <shivmarkad99@gmail.com>
2023-09-11 12:17:52 +0530

    [Shivganesh] Feat: totalCartPrice will be updated with addition of new book

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -6,27 +6,30 @@ export const addBookToCart = async (req) =>{
     const user_id = req.body.user_id;
 
     const getBook = await Book.findOne({_id: bookId})
-    if(getBook == null || getBook.quantity){ throw new Error("Book not availble or invalid book id") }
+    if(getBook == null || getBook.quantity <= 0 ){ throw new Error("Book not availble or invalid book id") }
 
     const getUserCart = await Cart.findOne({userId: user_id})
 
+    let totalCartPrice;
+
     if (getUserCart == null){
         const books = [{
-            productId: getBook._id, 
-            description: getBook.description, 
+            productId: getBook._id,
+            description: getBook.description,
             bookImage: getBook.bookImage, 
             bookName: getBook.bookName, 
             Author: getBook.Author, 
             price: getBook.price,
             totalPrice: getBook.price
         }]
-        const userNewCart = {userId: user_id, books:books}
+        const userNewCart = {userId: user_id, books:books, totalCartPrice: getBook.price}
         const createNewCart = await Cart.create(userNewCart)
         return createNewCart;
     }else{
         const cartBooks = getUserCart.books;
         let bookIndex = 0;
         let isPresent = false;
+
         for(let i = 0;i<cartBooks.length;i++){
             if(bookId == cartBooks[i].productId){
                 bookIndex = i;
@@ -34,11 +37,11 @@ export const addBookToCart = async (req) =>{
             }
         }
         if(isPresent == true){
-
             cartBooks[bookIndex].totalPrice = cartBooks[bookIndex].totalPrice + getBook.price
             cartBooks[bookIndex].quantity = cartBooks[bookIndex].quantity + 1;
+            totalCartPrice = getUserCart.totalCartPrice +  cartBooks[bookIndex].price;
 
-            const updateCart = await Cart.findByIdAndUpdate({_id:getUserCart._id}, { books: cartBooks}, { new: true })
+            const updateCart = await Cart.findByIdAndUpdate({_id:getUserCart._id}, { books: cartBooks, totalCartPrice: totalCartPrice}, { new: true })
             return updateCart;
         }else{
             const newBook = {
@@ -51,7 +54,8 @@ export const addBookToCart = async (req) =>{
                 totalPrice: getBook.price
             }
             cartBooks.push(newBook)
-            const addNewBookToCart = await Cart.findByIdAndUpdate({_id:getUserCart._id}, {books: cartBooks}, { new: true })
+            totalCartPrice = getUserCart.totalCartPrice + getBook.price;
+            const addNewBookToCart = await Cart.findByIdAndUpdate({_id:getUserCart._id}, {books: cartBooks,totalCartPrice: totalCartPrice}, { new: true })
             return addNewBookToCart;
         }
     }
