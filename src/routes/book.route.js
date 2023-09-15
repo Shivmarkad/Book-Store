5615d59e3b730aa7f876bbc0a15ab074acfd188a
Shivganesh <shivmarkad99@gmail.com>
2023-09-15 16:50:23 +0530

    [Shivganesh] Refactor: sorting parameter will be taken from the path as desc or asc

--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('', userAuth, booksController.getAllBooks);
 
 // to get sorted books in ascending
-router.get('/sort', userAuth, booksController.getSortedBooks);
+router.get('/:sort', userAuth, booksController.getSortedBooks);
 
 // to get sorted books in ascending
 router.get('/search', userAuth, booksController.searchBook);
