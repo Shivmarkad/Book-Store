10f3c0f525a19c1d730d28fe7f4bea38edd48b23
Shivganesh <shivmarkad99@gmail.com>
2023-10-04 15:12:43 +0530

    [Shivganesh] Added: transport to send mail using google OAuth2

---
+++ src/utils/mail.js
@@ -0,0 +1,48 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import logger from '../config/logger';
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+  try {
+    const oauth2Client = new OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        "https://developers.google.com/oauthplayground"
+      );
+
+      oauth2Client.setCredentials({
+        refresh_token: process.env.REFRESH_TOKEN,
+      });
+
+      const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+          if (err) {
+            logger.info("ERROR: ", err)
+            reject();
+          }
+          resolve(token); 
+        });
+      });
+
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: process.env.USER_EMAIL,
+          accessToken,
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRET,
+          refreshToken: process.env.REFRESH_TOKEN,
+         
+        }, tls: {
+          rejectUnauthorized: false
+        }
+      });
+      return transporter;
+  } catch (err) {
+    return err
+  }
+};
