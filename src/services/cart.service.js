5835d61f318d10f8bf0ac0523608893f4df04d13
Shivganesh <shivmarkad99@gmail.com>
2023-10-24 16:28:34 +0530

    [Shivganesh] Refactor: addBookToCart apis using higher order funtions for presence of book in cart

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -2,61 +2,52 @@ import Cart from '../models/cart.model';
 import Book from '../models/book.model';
 
 export const addBookToCart = async (user_id, bookId) => {
-
     const getBook = await Book.findOne({ _id: bookId })
-    if (getBook == null || getBook.quantity <= 0) { throw new Error("Book not availble or invalid book id") }
-
-    const getUserCart = await Cart.findOne({ userId: user_id })
-
-    let totalCartPrice;
-
-    if (getUserCart == null) {
-        const books = [{
-            productId: getBook._id,
-            description: getBook.description,
-            bookImage: getBook.bookImage,
-            bookName: getBook.bookName,
-            Author: getBook.Author,
-            price: getBook.price,
-            totalPrice: getBook.price
-        }]
-        const userNewCart = { userId: user_id, books: books, totalCartPrice: getBook.price }
-        const createNewCart = await Cart.create(userNewCart)
-        return createNewCart;
-    } else {
-        const books = getUserCart.books;
-        let bookIndex = 0;
-        let isPresent = false;
-
-        for (let i = 0; i < books.length; i++) {
-            if (bookId == books[i].productId) {
-                bookIndex = i;
-                isPresent = true;
-            }
-        }
-        if (isPresent == true) {
-            books[bookIndex].totalPrice = books[bookIndex].totalPrice + getBook.price
-            books[bookIndex].quantity = books[bookIndex].quantity + 1;
-            totalCartPrice = getUserCart.totalCartPrice + books[bookIndex].price;
-
-            const updateCart = await Cart.findByIdAndUpdate({ _id: getUserCart._id }, { books: books, totalCartPrice: totalCartPrice }, { new: true })
-            return updateCart;
-        } else {
-            const newBook = {
-                productId: getBook._id,
-                description: getBook.description,
-                bookImage: getBook.bookImage,
-                bookName: getBook.bookName,
-                Author: getBook.Author,
-                price: getBook.price,
-                totalPrice: getBook.price
-            }
-            books.push(newBook)
-            totalCartPrice = getUserCart.totalCartPrice + getBook.price;
-            const addNewBookToCart = await Cart.findByIdAndUpdate({ _id: getUserCart._id }, { books: books, totalCartPrice: totalCartPrice }, { new: true })
-            return addNewBookToCart;
-        }
+    if (getBook == null || getBook.quantity <= 0) {
+        throw new Error("Book not available or invalid book id")
     }
+    const getUserCart = await Cart.findOne({ userId: user_id })
+    let totalCartPrice;
+    
+    const newBook = {
+        productId: getBook._id,
+        description: getBook.description,
+        bookImage: getBook.bookImage,
+        bookName: getBook.bookName,
+        Author: getBook.Author,
+        price: getBook.price,
+        totalPrice: getBook.price
+    }
+    if (getUserCart == null) {
+        const createNewCart = await Cart.create({
+            userId: user_id,
+            books: [newBook],
+            totalCartPrice: getBook.price
+        })
+        return createNewCart;
+    }
+    const isBookPresent = getUserCart.books.filter(book => book.productId == bookId);
+    if (isBookPresent.length > 0) {
+        const books = getUserCart.books.map(item => {
+            if (item.productId == bookId) {
+                item.totalPrice = item.totalPrice + getBook.price;
+                item.quantity += 1
+                totalCartPrice = getUserCart.totalCartPrice + item.price;
+            }
+            return item;
+        })
+        const updateCart = await Cart.findByIdAndUpdate({ _id: getUserCart._id },
+            { books: books, totalCartPrice: totalCartPrice },
+            { new: true })
+        return updateCart;
+    }
+    getUserCart.books.push(newBook)
+    totalCartPrice = getUserCart.totalCartPrice + getBook.price;
+
+    const addNewBookToCart = await Cart.findByIdAndUpdate({ _id: getUserCart._id },
+        { books: getUserCart.books, totalCartPrice: totalCartPrice },
+        { new: true })
+    return addNewBookToCart;
 }
 
 export const deleteBookFromCart = async (user_id, bookId) => {
@@ -97,9 +88,9 @@ export const purchaseOrders = async (user_id) => {
 
     let purchaseStatus;
     getUserCart.isPurchased ? purchaseStatus = false : purchaseStatus = true;
-
-    const purchaseCartBooks = await Cart.findByIdAndUpdate({ _id: getUserCart._id }, { isPurchased: purchaseStatus }, { new: true });
-
+    const purchaseCartBooks = await Cart.findByIdAndUpdate({ _id: getUserCart._id },
+        { isPurchased: purchaseStatus },
+        { new: true });
     if (purchaseCartBooks) { return purchaseCartBooks }
     throw new Error("Unable to purchase server error")
 }
@@ -107,7 +98,7 @@ export const purchaseOrders = async (user_id) => {
 export const getCart = async (user_id) => {
     const getUserCart = await Cart.findOne({ userId: user_id })
     if (getUserCart == null) {
-        throw new Error("Cart not found or cart is empty") 
+        throw new Error("Cart not found or cart is empty")
     }
-   return getUserCart;
+    return getUserCart;
 }
